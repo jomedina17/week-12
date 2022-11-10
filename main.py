68f4355e52c7e54bf70e609dc1324ffbbfb561ae
Jocelyn Medina <113551944+jomedina17@users.noreply.github.com>
2022-11-10 20:42:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+
+#   Interactions Between Functions Practice #1
+# Create a function (throw_dice) that "throws" two random dice and returns its results (the function MUST RETURN TWO VALUES AS A RESULT, both of which must be between 1 and 6, randomly).
+throw_dice = 
+
+return
+
+
+# Pass the result of these two dice to a function called roll_result (meaning that this second function MUST RECEIVE TWO ARGUMENTS) and return -without printing it- a certain message according to the what the sum of these values results:
+
+# If the sum is less than or equal to 6:
+
+# "The sum of your dice is {sum_dice}. Unfortunate"
+if sum <= 6:
+  print("unfortunate")
+# If the sum is greater than 6 and less than 10:
+if sum > 6 < 10:
+  print("you have a good chance")
+# "The sum of your dice is {sum_dice}. You have a good chance"
+
+# If the sum is greater than or equal to 10:
+
+# "The sum of your dice is {sum_dice}. It looks like a winning roll"
+  if sum >= 10:
+    print("it looks like a winning roll")
+
+# Hint: use the random library's choice or randint method to choose a random value between 1 and 6.
+
+#   "The sum of your dice is {suma_dados}. Unfortunate"
+# "The sum of your dice is {suma_dados}. You have a good chance"
+# "The sum of your dice is {sum_dice}. It looks like a winning roll"
+
+
+
+# Create a function called reduce_list() that takes a list (numbers) as an argument, and returns also a list, but removing duplicates (leaving only one of the numbers if there are duplicates) and removing the highest value. The order of the elements can be changed.
+
+# For example, if given the list [1,2,15,7,2] it should return [1,2,7].
+
+# Create a function called average() that can receive as an argument the list returned by the previous function, and that calculates the average of its values. It should return the result (a float), without printing it.
+
+
+
+
+
